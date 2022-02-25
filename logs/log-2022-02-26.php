d390e96b7b35ba3a8311e48b99e3947a200f4294
yu jin <galih11120@gmail.com>
2022-02-26 01:10:01 +0700

    first commit

---
+++ logs/log-2022-02-26.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-02-26 01:06:55 --> Unable to connect to the database
+ERROR - 2022-02-26 01:06:55 --> Severity: Core Warning --> PHP Startup: Unable to load dynamic library 'gd2' (tried: /usr/lib/php/20190902/gd2 (/usr/lib/php/20190902/gd2: cannot open shared object file: No such file or directory), /usr/lib/php/20190902/gd2.so (/usr/lib/php/20190902/gd2.so: cannot open shared object file: No such file or directory)) Unknown 0
